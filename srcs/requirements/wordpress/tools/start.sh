6c1664468e0822a573053e4f2fd46402cf18baaf
Sungwook Kim <silenc3@naver.com>
2024-04-24 18:42:06 +0900

    fix: js error

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -6,7 +6,7 @@ if [ ! -e "/var/www/html/wp-config.php" ]; then
 		cd /var/www/html
 		wp core download --allow-root
 		wp config create --dbname=$WP_DB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_ROOT_PASSWORD --dbhost=$MARIADB_HOST --skip-check
-		wp core install --url=$WP_URL/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+		wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 		wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
 fi
 
