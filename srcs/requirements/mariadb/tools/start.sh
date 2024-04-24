b43e0ea380e6f72b9fc09a749de28d4941c636be
Sungwook Kim <silenc3@naver.com>
2024-04-24 16:37:42 +0900

    fix: 환경변수 오류

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,28 +1,25 @@
 #!/bin/sh
 
 if [ ! -e "/run/mysqld/mysqld.sock" ]; then
-    #https://mariadb.com/kb/en/mariadb-install-db/
-    #mariadb-install-db initializes the MariaDB data directory and creates the system tables in the mysql database, if they do not exist
-    #시스템 테이블 설치 안하면
-    #mysqld_safe Starting mariadbd daemon with databases from /var/lib/mysql
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
-    mkdir -p /run/mysqld
-    mysqld &
+		#https://mariadb.com/kb/en/mariadb-install-db/
+		#mariadb-install-db initializes the MariaDB data directory and creates the system tables in the mysql database, if they do not exist
+		#시스템 테이블 설치 안하면
+		#mysqld_safe Starting mariadbd daemon with databases from /var/lib/mysql
+		mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+		mkdir -p /run/mysqld
+		mysqld &
 
-    #https://stackoverflow.com/questions/25503412/how-do-i-know-when-my-docker-mysql-container-is-up-and-mysql-is-ready-for-taking
-    while ! mysqladmin ping; do
-        sleep 1
-    done
+		#https://stackoverflow.com/questions/25503412/how-do-i-know-when-my-docker-mysql-container-is-up-and-mysql-is-ready-for-taking
+		while ! mysqladmin ping; do
+				sleep 1
+		done
 
-    # root 사용자 암호 설정
-    mysql -uroot --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-    # 사용자에게 데이터베이스에 대한 모든 접근 권한 부여
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-    # 데이터베이스 생성
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-    # 권한 새로고침
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-    mysqladmin -uroot -p"$MYSQL_ROOT_PASSWORD" shutdown
+		# root 사용자 암호 설정
+		mysql -uroot --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
+		mysql -uroot -p"$MARIADB_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
+		mysql -uroot -p"$MARIADB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;"
+		mysql -uroot -p"$MARIADB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+		mysqladmin -uroot -p"$MARIADB_ROOT_PASSWORD" shutdown
 fi
 
 exec mysqld
--- srcs/requirements/nginx/tools/start.sh
+++ srcs/requirements/nginx/tools/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ ! -d "/etc/nginx/ssl" ]; then
-    mkdir /etc/nginx/ssl
-    # 인증서 요청을 생성
-    # rsa:4096 4096비트의 키를 생성, -x509: 자체 서명된 인증서를 생성하기 위한 옵션
-    openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
-            -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Student/CN=sungwook" \
-            -keyout /etc/nginx/ssl/sungwook.key -out /etc/nginx/ssl/sungwook.crt
-    mkdir -p /run/nginx
+		mkdir /etc/nginx/ssl
+		# 인증서 요청을 생성
+		# rsa:4096 4096비트의 키를 생성, -x509: 자체 서명된 인증서를 생성하기 위한 옵션
+		openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
+						-subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Student/CN=sungwook" \
+						-keyout /etc/nginx/ssl/sungwook.key -out /etc/nginx/ssl/sungwook.crt
+		mkdir -p /run/nginx
 fi
 
 # nginx를 포그라운드에서 실행
